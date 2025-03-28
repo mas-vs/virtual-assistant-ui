0199daa080c4c0e52ddf5fcbfb7b68e147384bf2
valuespace-dev <admin@valuespace.ai>
2025-03-28 10:28:37 +0100

    No iframe

--- js/web-chat.mjs
+++ js/web-chat.mjs
@@ -2,18 +2,6 @@ import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bun
 
 console.log(`%cValueSpace.ai\n%cAI-powered hotel chatbot, \u00A9 ${(new Date).getFullYear()} ValueSpace.ai\n visit https://valuespace.ai`, "color:#c93656;font-weight:bolder;font-family:Montserrat,sans-serif;font-size:40px;text-shadow:-1px 0 #1b2f5d,0 1px #1b2f5d,1px 0 #1b2f5d,0 -1px #1b2f5d;", "font-weight:bolder;");
 
-window.addEventListener('DOMContentLoaded', () => {
-  const styles = getComputedStyle(document.documentElement);
-  const height = styles.getPropertyValue('--chat--window--height').trim() || '600px';
-  const width = styles.getPropertyValue('--chat--window--width').trim() || '400px';
-
-  parent.postMessage({
-    type: 'chat-set-size',
-    height,
-    width
-  }, '*');
-});
-
 // Function to dynamically inject the modalImageViewer widget
 function injectModalViewer() {
 	if (document.getElementById('modalImageViewer')) return; // Prevent duplicate injection
@@ -21,20 +9,11 @@ function injectModalViewer() {
 		<div class="modal" style="display: none;">
 			<div class="modal-image-viewer" id="modalImageViewer">
 				<div class="demo-nav">
-					<button class="demo-nav-rotate active">
-						<span>Rotate</span>
-					</button>
-					<button class="demo-nav-theme">
-						<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor">
-							<path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z" />
-						</svg>
-					</button>
 					<button class="demo-nav-content">
 						<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor">
 							<path d="M120-240v-80h480v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
 						</svg>
 					</button>
-					<button class="toggle-nav-buttons active"><></button>
 				</div>
 				<div class="swiper">
 					<div class="swiper-wrapper" id="swiperWrapper"></div>
@@ -99,7 +78,23 @@ let currentImageIndex = 0;
 let images = [];
 let swiper;
 
+
+document.addEventListener('click', function (event) {
+	if (event.target.tagName === 'IMG' && event.target.closest('.chat-message')) {
+		images = Array.from(document.querySelectorAll('.chat-message img'));
+		const clickedImageIndex = images.indexOf(event.target);
+		parent.openModalImageViewer(clickedImageIndex);
+	} else if (event.target.closest('.more-images')) {
+		images = Array.from(document.querySelectorAll('.chat-message img'));
+		parent.openModalImageViewer(3);
+	}
+});
+
+injectModalViewer();
+
+
 window.openModalImageViewer = function (index) {
+	console.log("openModalImageViewer");
 	currentImageIndex = index;
 	const swiperWrapper = document.getElementById('swiperWrapper');
 	swiperWrapper.innerHTML = ''; // Clear existing slides
@@ -135,22 +130,10 @@ window.openModalImageViewer = function (index) {
 	}
 
 	const chatWindowToggle = document.querySelector('.chat-window-toggle');
-
-	const buttonSwitchArrows = document.querySelector('.toggle-nav-buttons');
-	if (!buttonSwitchArrows.classList.contains('active')) {
-		buttonSwitchArrows.click();
-	}
-
-	if (chatWindowToggle) chatWindowToggle.click();
 }
 
 window.closeModalImageViewer = function () {
 	const chatWindowToggle = document.querySelector('.chat-window-toggle');
-
-	const buttonSwitchArrows = document.querySelector('.toggle-nav-buttons');
-	if (buttonSwitchArrows.classList.contains('active')) {
-		buttonSwitchArrows.click();
-	}
 
 	const swiperWrapper = document.getElementById('swiperWrapper');
 	if (swiperWrapper) {
@@ -165,40 +148,7 @@ window.closeModalImageViewer = function () {
 	if (chatWindowToggle) chatWindowToggle.click();
 }
 
-document.addEventListener('click', function (event) {
-	if (event.target.tagName === 'IMG' && event.target.closest('.chat-message')) {
-		images = Array.from(document.querySelectorAll('.chat-message img'));
-		const clickedImageIndex = images.indexOf(event.target);
-		openModalImageViewer(clickedImageIndex);
-	} else if (event.target.closest('.more-images')) {
-		images = Array.from(document.querySelectorAll('.chat-message img'));
-		openModalImageViewer(3);
-	}
-});
 
-injectModalViewer();
-
-document.addEventListener('click', function (event) {
-	const modalContainer = document.querySelector('.modal');
-	if (modalContainer && event.target === modalContainer && mouseDownTarget === modalContainer) {
-		closeModalImageViewer();
-		mouseDownTarget = null; // Reset mouseDownTarget to null
-	}
-});
-
-const chatMessagesContainer = document.querySelector('.chat-messages-list');
-const observer = new MutationObserver(() => {
-	images = Array.from(document.querySelectorAll('.chat-message img'));
-	groupImages();
-});
-
-observer.observe(chatMessagesContainer, { childList: true, subtree: true });
-
-let mouseDownTarget = null;
-
-document.addEventListener('mousedown', function (event) {
-	mouseDownTarget = event.target;
-});
 
 // Initialize Swiper only if it's not already initialized
 if (!swiper) {
@@ -236,31 +186,12 @@ if (!swiper) {
 		},
 	});
 
+	// light theme
+	document.body.classList.toggle('light')
+	// dark theme
+	//document.body.classList.toggle('content-visible')
+
 	SwitchToHorizontalView();
-
-	document.querySelector('.demo-nav-theme').addEventListener('click', (e) => {
-		document.body.classList.toggle('light')
-		e.target.closest('button').classList.toggle('active')
-	})
-	document.querySelector('.demo-nav-content').addEventListener('click', (e) => {
-		document.body.classList.toggle('content-visible')
-		e.target.closest('button').classList.toggle('active')
-	})
-
-	const buttonSwitchArrows = document.querySelector('.toggle-nav-buttons');
-	buttonSwitchArrows.addEventListener('click', () => {
-		if (buttonSwitchArrows.classList.contains('active')) {
-			buttonSwitchArrows.classList.remove('active');
-		}
-		else {
-			buttonSwitchArrows.classList.add('active');
-		}
-
-		const navButtons = document.querySelectorAll('.swiper-button-next, .swiper-button-prev');
-		navButtons.forEach(button => {
-			button.style.display = button.style.display === 'none' ? 'flex' : 'none';
-		});
-	});
 
 	// Ensure buttons are hidden on small screens
 	const mediaQuery = window.matchMedia('(max-width: 768px)');
@@ -274,9 +205,6 @@ if (!swiper) {
 		} else {
 			navButtons.forEach(button => {
 				button.style.display = 'flex';
-				if (buttonSwitchArrows.classList.contains('active')) {
-					button.style.display = 'flex';
-				}
 			});
 			SwitchToHorizontalView();
 		}
@@ -301,46 +229,70 @@ if (!swiper) {
 	}
 
 
-	function groupImages() {
-		const imageContainers = document.querySelectorAll('.chat-message-markdown ol');
-		imageContainers.forEach(container => {
-			const imageElements = container.querySelectorAll('li img');
-			if (imageElements.length >= 4) {
-				const imageGrid = document.createElement('div');
-				imageGrid.className = 'image-grid';
+	document.addEventListener('click', function (event) {
+		const modalContainer = document.querySelector('.modal');
+		if (modalContainer && event.target === modalContainer && mouseDownTarget === modalContainer) {
+			closeModalImageViewer();
+			mouseDownTarget = null; // Reset mouseDownTarget to null
+		}
+	});
 
-				for (let i = 0; i < 4; i++) {
-					const imgWrapper = document.createElement('div');
-					if (i === 3 && imageElements.length > 4) {
-						imgWrapper.className = 'more-images';
-						imgWrapper.setAttribute('data-count', `+${imageElements.length - 3}`);
-					}
-					imgWrapper.appendChild(imageElements[i].cloneNode(true));
-					imageGrid.appendChild(imgWrapper);
+};
+
+
+function groupImages() {
+	const imageContainers = document.querySelectorAll('.chat-message-markdown ol');
+	imageContainers.forEach(container => {
+		const imageElements = container.querySelectorAll('li img');
+		if (imageElements.length >= 4) {
+			const imageGrid = document.createElement('div');
+			imageGrid.className = 'image-grid';
+
+			for (let i = 0; i < 4; i++) {
+				const imgWrapper = document.createElement('div');
+				if (i === 3 && imageElements.length > 4) {
+					imgWrapper.className = 'more-images';
+					imgWrapper.setAttribute('data-count', `+${imageElements.length - 3}`);
 				}
-
-				container.innerHTML = '';
-				container.appendChild(imageGrid);
-
-				const hiddenImages = document.createElement('div');
-				hiddenImages.className = 'hidden-images';
-				for (let i = 4; i < imageElements.length; i++) {
-					hiddenImages.appendChild(imageElements[i].cloneNode(true));
-				}
-				container.appendChild(hiddenImages);
-			} else {
-				imageElements.forEach(img => {
-					if (!img.closest('.image-grid')) {
-						const altText = img.alt;
-						if (altText) {
-							const caption = document.createElement('div');
-							caption.className = 'image-caption';
-							caption.textContent = altText;
-							img.insertAdjacentElement('afterend', caption);
-						}
-					}
-				});
+				imgWrapper.appendChild(imageElements[i].cloneNode(true));
+				imageGrid.appendChild(imgWrapper);
 			}
-		});
-	}
-};	
+
+			container.innerHTML = '';
+			container.appendChild(imageGrid);
+
+			const hiddenImages = document.createElement('div');
+			hiddenImages.className = 'hidden-images';
+			for (let i = 4; i < imageElements.length; i++) {
+				hiddenImages.appendChild(imageElements[i].cloneNode(true));
+			}
+			container.appendChild(hiddenImages);
+		} else {
+			imageElements.forEach(img => {
+				if (!img.closest('.image-grid')) {
+					const altText = img.alt;
+					if (altText) {
+						const caption = document.createElement('div');
+						caption.className = 'image-caption';
+						caption.textContent = altText;
+						img.insertAdjacentElement('afterend', caption);
+					}
+				}
+			});
+		}
+	});
+}
+
+const chatMessagesContainer = document.querySelector('.chat-messages-list');
+const observer = new MutationObserver(() => {
+	images = Array.from(document.querySelectorAll('.chat-message img'));
+	groupImages();
+});
+
+observer.observe(chatMessagesContainer, { childList: true, subtree: true });
+
+let mouseDownTarget = null;
+
+document.addEventListener('mousedown', function (event) {
+	mouseDownTarget = event.target;
+});
